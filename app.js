0dbd023c06b47599e9d77973dc8c8f6754b6f097
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-11-23 21:41:33 +0100

    Preconfigurado i18n. eslint eliminado.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const i18n = require('./lib/i18nConfiguration');
 
 //Routes
 var indexRouter = require('./routes/index');
@@ -15,7 +16,9 @@ var app = express();
 /* global __dirname */
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.set('view engine', 'html'); // usa un motor de vista custom, llamado 'html'
+app.engine('html', require('ejs').__express); // ese motor usa ejs
 
 //connectMongoose import
 require('./lib/connectMongoose');
@@ -27,10 +30,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //icon request is ignored
-app.use('/favicon.ico', (req, res, next) =>{
+app.use('/favicon.ico', (req, res, next) => {
   res.status(204);
   res.send();
 });
+
+app.use(i18n.init);
 
 /* API request */
 app.use('/api', apiRouter);
@@ -38,32 +43,33 @@ app.use('/api', apiRouter);
 /* Web request */
 app.use('/', indexRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404, 'Sorry, this page doesn\'t exist'));
+app.use(function (req, res, next) {
+  next(createError(404, "Sorry, this page doesn't exist"));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.url= req.url;
-  
+  res.locals.url = req.url;
+
   //422 message error handler
-  if (err.status===422) {
-    if (err.errors.length===1) {
+  if (err.status === 422) {
+    if (err.errors.length === 1) {
       res.locals.message = err.errors[0].msg;
     } else {
-      res.locals.message='Several errors in request:';
-      err.errors.forEach(error => {
-        res.locals.message+= ' '+error.msg+'; ';
+      res.locals.message = 'Several errors in request:';
+      err.errors.forEach((error) => {
+        res.locals.message += ' ' + error.msg + '; ';
       });
     }
-  } else {res.locals.message = err.message;}
+  } else {
+    res.locals.message = err.message;
+  }
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  err.status=err.status || 500;
+  err.status = err.status || 500;
   res.status(err.status);
   res.render('error');
 });
